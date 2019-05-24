5a37e9e1b93b24be50ba71ec919aa08fb291078a
Lucas <lucas@lindenau.informatik.uni-leipzig.de>
2019-05-24 13:19:26 +0200

    Implement efficent solution

--- src/main/java/TermsOfSum.java
+++ src/main/java/TermsOfSum.java
@@ -1,19 +1,23 @@
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
+import java.util.Map;
 
 public class TermsOfSum {
     public static Integer[] execute(List<Integer> c1, List<Integer> c2, Integer sum) {
 
       Integer[] result = new Integer[2];
 
-      for (Integer i1 : c1) {
-        for (Integer i2 : c2) {
-          if (Objects.equals((i1 + i2), sum)) {
-            result[0] = i1;
-            result[1] = i2;
-          }
-        }
+      Map<Integer, Integer> s = new HashMap<>();
+
+      for (int i1 : c1) {
+        s.put(i1, 0);
       }
-    return result;
-  }
+
+      for (int i2 : c2) {
+        if (s.containsKey(sum - i2))
+          return new Integer[]{sum - i2, i2};
+      }
+
+      return result;
+    }
 }
