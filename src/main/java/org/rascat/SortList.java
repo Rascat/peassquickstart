a63b6bae943e5f5daba9892567993c641f58eb5e
Lucas Schons <schons@studserv.uni-leipzig.de>
2019-06-28 13:34:36 +0200

    Delay sorting by 6s

--- src/main/java/org/rascat/SortList.java
+++ src/main/java/org/rascat/SortList.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class SortList {
   public static List<Integer> execute(List<Integer> list) throws InterruptedException {
     list.sort(Comparator.naturalOrder());
-    Thread.sleep(3000);
+    Thread.sleep(6000);
     return list;
   }
 }
